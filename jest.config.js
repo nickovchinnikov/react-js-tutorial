d9808a62b0c46ac9d558fad56ed51fb6e26a14a8
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2020-03-30 12:32:42 +0300

    Add lesson 1

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path")
+
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+}
