f8aded7b1e6b7c6b04cba503cd4fa0af48a5369f
Nikita Ovchinnikov <nickovchinnikof@gmail.com>
2020-04-09 12:09:24 +0300

    Add react to project Settings for eslint Settings for Jest

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/internals/jestSettings.js"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
