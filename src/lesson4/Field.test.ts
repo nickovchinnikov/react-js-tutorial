f46e727c26389ee65db602933b2c87e68d5e2338
Vasil Vanchuk <v.vanchuk@tut.by>
2020-04-12 04:59:34 +0300

    Add test for field component to render filled cells (element)

--- src/lesson4/Field.test.ts
+++ src/lesson4/Field.test.ts
@@ -2,7 +2,7 @@ import { mount } from "enzyme";
 import { getFieldElement } from "./Field";
 
 describe("Field", () => {
-  it("renders cess for passed empty field", () => {
+  it("renders cells for passed empty field", () => {
     const field = mount(
       getFieldElement({
         field: [
@@ -15,5 +15,30 @@ describe("Field", () => {
     expect(field.find("br").length).toBe(1); // rows - 1
     expect(field.find(".cell").length).toBe(4); // cell
     expect(field.find(".cell-empty").length).toBe(4); // cell
+  });
+  it("renders filled cells", () => {
+    const field = mount(
+      getFieldElement({
+        field: [
+          ["x", "o"],
+          ["o", ""],
+        ],
+        onClick: jest.fn(),
+      })
+    );
+    expect(field.find("br").length).toBe(1);
+    expect(field.find(".cell").length).toBe(4);
+    expect(field.find(".cell-empty").length).toBe(1);
+    expect(field.find(".cell-filled").length).toBe(3);
+    expect(
+      field.findWhere(
+        (el) => el.text() === "x" && typeof el.type() !== "string"
+      ).length
+    ).toBe(1);
+    expect(
+      field.findWhere(
+        (el) => el.text() === "o" && typeof el.type() !== "string"
+      ).length
+    ).toBe(2);
   });
 });
