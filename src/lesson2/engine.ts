ccdfd851cc18a836db97439a99fd8da3062da62f
Чернецова Татьяна <chernetsova.tatya@gmail.com>
2020-04-15 19:11:43 +0300

    fixes by code review

--- src/lesson2/engine.ts
+++ src/lesson2/engine.ts
@@ -1,67 +1,65 @@
 import { ParsedLineType } from "./parser";
 import { isNumber } from "./helpers";
 import {
-  mathPriorities,
-  mathOperatorsPriorities,
-  binaryMathOperators,
-  unaryMathOperators,
+	mathPriorities,
+	mathOperatorsPriorities,
+	binaryMathOperators,
+	unaryMathOperators,
 } from "./mathOperators";
 
 const [ZERO, FIRST, SECOND] = mathPriorities;
 
 export const zeroPrioritiesCalc = (stack: ParsedLineType): ParsedLineType =>
-  stack.reduce<ParsedLineType>((result, item) => {
-    const prevItem = result[result.length - 1];
+	stack.reduce<ParsedLineType>((result, item) => {
+		const prevItem = result[result.length - 1];
 
-    if (!isNumber(String(item)) && mathOperatorsPriorities[item] === ZERO) {
-      if (!unaryMathOperators[item]) {
-        throw new TypeError("Unexpected stack!");
-      }
-      if (prevItem) {
-        result = [
-          ...result.slice(0, -1),
-          unaryMathOperators[item](Number(prevItem)),
-        ];
-      }
-    } else {
-      result.push(item);
-    }
+		if (!isNumber(String(item)) && mathOperatorsPriorities[item] === ZERO) {
+			if (!unaryMathOperators[item]) {
+				throw new TypeError("Unexpected stack!");
+			}
+			if (prevItem) {
+				result = [
+					...result.slice(0, -1),
+					unaryMathOperators[item](Number(prevItem)),
+				];
+			}
+		} else {
+			result.push(item);
+		}
 
-    return result;
-  }, []);
+		return result;
+	}, []);
 
 export const firstPrioritiesCalc = (stack: ParsedLineType): ParsedLineType =>
-  stack.reduce<ParsedLineType>((result, nextItem) => {
-    const prevItem = result[result.length - 2];
-    const item = result[result.length - 1];
+	stack.reduce<ParsedLineType>((result, nextItem) => {
+		const prevItem = result[result.length - 2];
+		const item = result[result.length - 1];
 
-    if (!isNumber(String(item)) && mathOperatorsPriorities[item] === FIRST) {
-      if (!binaryMathOperators[item]) {
-        throw new TypeError("Unexpected stack!");
-      }
-      result = [
-        ...result.slice(0, -2),
-        binaryMathOperators[item](Number(prevItem), Number(nextItem)),
-      ];
-    } else {
-      result.push(nextItem);
-    }
-    return result;
-  }, []);
+		if (!isNumber(String(item)) && mathOperatorsPriorities[item] === FIRST) {
+			if (!binaryMathOperators[item]) {
+				throw new TypeError("Unexpected stack!");
+			}
+			result = [
+				...result.slice(0, -2),
+				binaryMathOperators[item](Number(prevItem), Number(nextItem)),
+			];
+		} else {
+			result.push(nextItem);
+		}
+		return result;
+	}, []);
 
 export const secondPrioritiesCalc = (stack: ParsedLineType): number =>
-  stack.reduce<number>((result, nextItem, key) => {
-    const item = stack[key - 1];
+	stack.reduce<number>((result, nextItem, key) => {
+		const item = stack[key - 1];
 
-    if (item) {
-      if (!isNumber(String(item))) {
-        if (mathOperatorsPriorities[item] !== SECOND) {
-          throw new TypeError("Unexpected stack!");
-        }
+		if (item && !isNumber(String(item))) {
+			if (mathOperatorsPriorities[item] !== SECOND) {
+				throw new TypeError("Unexpected stack!");
+			}
 
-        result = binaryMathOperators[item](Number(result), Number(nextItem));
-      }
-    }
+			result = binaryMathOperators[item](Number(result), Number(nextItem));
+		}
 
-    return result;
-  }, Number(stack[0]));
+		return result;
+	}, Number(stack[0]));
--- src/lesson2/mathOperators.ts
+++ src/lesson2/mathOperators.ts
@@ -1,70 +1,54 @@
 export type BinaryOperationType = (first: number, second: number) => number;
 export type UnaryOperationType = (argument: number) => number;
 
-export const mul: BinaryOperationType = (
-  first: number,
-  second: number
-): number => first * second;
+export const mul: BinaryOperationType = (first, second) => first * second;
 
-export const div: BinaryOperationType = (
-  first: number,
-  second: number
-): number => first / second;
+export const div: BinaryOperationType = (first, second) => first / second;
 
-export const add: BinaryOperationType = (
-  first: number,
-  second: number
-): number => first + second;
+export const add: BinaryOperationType = (first, second) => first + second;
 
-export const minus: BinaryOperationType = (
-  first: number,
-  second: number
-): number => first - second;
+export const minus: BinaryOperationType = (first, second) => first - second;
 
-export const power: BinaryOperationType = (
-  first: number,
-  second: number
-): number => first ** second;
+export const power: BinaryOperationType = (first, second) => first ** second;
 
-export const square: UnaryOperationType = (argument: number): number =>
-  power(argument, 2);
+export const square: UnaryOperationType = (argument) => power(argument, 2);
 
-export const factorial: UnaryOperationType = (argument: number): number => {
-  let result = argument;
+export const factorial: UnaryOperationType = (argument) => {
+	let result = argument;
 
-  if (argument < 0) {
-    throw TypeError(
-      "The factorial function is only defined for nonnegative values"
-    );
-  }
+	if (argument < 0) {
+		throw TypeError(
+			"The factorial function is only defined for nonnegative values"
+		);
+	}
 
-  if (argument === 0 || argument === 1) return 1;
-  while (argument > 1) {
-    argument--;
-    result *= argument;
-  }
-  return result;
+	if (argument === 0 || argument === 1) return 1;
+	while (argument > 1) {
+		argument--;
+		result *= argument;
+	}
+	return result;
 };
 
 export const binaryMathOperators: {
-  [key: string]: BinaryOperationType;
+	[key: string]: BinaryOperationType;
 } = {
-  "*": mul,
-  "/": div,
-  "+": add,
-  "-": minus,
-  "^": power,
+	"*": mul,
+	"/": div,
+	"+": add,
+	"-": minus,
+	"^": power,
 };
 
 export const unaryMathOperators: {
-  [key: string]: UnaryOperationType;
+	[key: string]: UnaryOperationType;
 } = {
-  "**": square,
-  "!": factorial,
+	"**": square,
+	"!": factorial,
 };
 
 export const mathOperators: {
-  [key: string]: UnaryOperationType | BinaryOperationType;
+	[key: string]: UnaryOperationType | BinaryOperationType;
 } = { ...unaryMathOperators, ...binaryMathOperators };
 
 export const mathPriorities: number[] = [0, 1, 2];
@@ -72,11 +56,11 @@ export const mathPriorities: number[] = [0, 1, 2];
 const [ZERO, FIRST, SECOND] = mathPriorities;
 
 export const mathOperatorsPriorities: { [key: string]: number } = {
-  "**": ZERO,
-  "!": ZERO,
-  "*": FIRST,
-  "/": FIRST,
-  "^": FIRST,
-  "+": SECOND,
-  "-": SECOND,
+	"**": ZERO,
+	"!": ZERO,
+	"*": FIRST,
+	"/": FIRST,
+	"^": FIRST,
+	"+": SECOND,
+	"-": SECOND,
 };
