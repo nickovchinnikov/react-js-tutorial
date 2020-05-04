ea52289c7f816a41e5512c69f55bf5fb5f83f575
Vasil Vanchuk <v.vanchuk@tut.by>
2020-05-04 13:22:30 +0300

    use class component

--- src/components/Entrance/Entrance.tsx
+++ src/components/Entrance/Entrance.tsx
@@ -1,14 +1,28 @@
 import React, { useState, useCallback } from "react";
 
-export const Entrance: React.FC<{ defaultValue: string }> = ({
-  defaultValue,
-}) => {
-  const [value, setValue] = useState(defaultValue);
-  const onChange = useCallback(
-    (ev) => {
-      setValue(ev.target.value.toUpperCase());
-    },
-    [setValue]
-  );
-  return <input type="text" value={value} onChange={onChange} />;
-};
+// export const Entrance: React.FC<{ defaultValue: string }> = ({
+//   defaultValue,
+// }) => {
+//   const [value, setValue] = useState(defaultValue);
+//   const onChange = useCallback(
+//     (ev) => {
+//       setValue(ev.target.value.toUpperCase());
+//     },
+//     [setValue]
+//   );
+//   return <input type="text" value={value} onChange={onChange} />;
+// };
+export class Entrance extends React.Component<{ defaultValue: string }, {}> {
+  state = {
+    value: this.props.defaultValue,
+  };
+
+  setValue = (ev: { target: HTMLInputElement }) =>
+    this.setState({ value: ev.target.value.toUpperCase() });
+
+  render() {
+    return (
+      <input type="text" value={this.state.value} onChange={this.setValue} />
+    );
+  }
+}
