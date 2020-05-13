7405399bd6e35bdc69525b5c70734441850f4bac
Vasil Vanchuk <v.vanchuk@tut.by>
2020-05-13 22:23:36 +0300

    Setup root alias

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     alias: {
       types: path.resolve(__dirname, "src/types"),
       components: path.resolve(__dirname, "src/components"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   output: {
