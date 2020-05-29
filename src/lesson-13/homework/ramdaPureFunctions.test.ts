2edda8253a27ff3525f288513e3f1d84d9727025
Vasil Vanchuk <v.vanchuk@tut.by>
2020-05-29 13:47:19 +0300

    Fix linting issues

--- src/lesson-13/homework/ramdaPureFunctions.test.ts
+++ src/lesson-13/homework/ramdaPureFunctions.test.ts
@@ -1,5 +1,4 @@
 test("dummy test", () => undefined);
-
 /*
 
 import {
@@ -10,32 +9,23 @@ import {
   parseQs,
 } from "./ramdaPureFunctions";
 
-test("getTopName", () => {
-  const teams: Team[] = [
-    { name: "Lions", score: 5 },
-    { name: "Tigers", score: 4 },
-    { name: "Bears", score: 6 },
-    { name: "Monkeys", score: 2 },
-  ];
+// });
 
-  expect(getTopName(teams)).toBe("Bears");
-});
+// test("createQs", () => {
+//   const qsObj: QsObj = {
+//     page: "2",
+//     pageSize: "10",
+//     total: "205",
+//     somethingElse: "value",
+//   };
 
-test("createQs", () => {
-  const qsObj: QsObj = {
-    page: "2",
-    pageSize: "10",
-    total: "205",
-    somethingElse: "value",
-  };
+//   expect(createQs(qsObj)).toBe(
+//     "?page=2&pageSize=10&total=205&somethingElse=value"
+//   );
+// });
 
-  expect(createQs(qsObj)).toBe(
-    "?page=2&pageSize=10&total=205&somethingElse=value"
-  );
-});
-
-test("parseQs", () => {
-  const qs = "?page=2&pageSize=10&total=205&somethingElse=value";
+// test("parseQs", () => {
+//   const qs = "?page=2&pageSize=10&total=205&somethingElse=value";
 
   expect(parseQs(qs)).toBe({
     page: "2",
