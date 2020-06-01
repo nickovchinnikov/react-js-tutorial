9900e3bb1a56f9bbd71aced7cb3a28d29c994a75
Michael Bashurov <saitonakamura@wisebits.com>
2020-06-01 13:04:40 +0300

    Add lesson 17 homework

---
+++ src/lesson17/homework/asyncFlow.ts
@@ -0,0 +1,18 @@
+/*
+
+Курс React, урок 17: Middlewares
+
+Домашнее задание 1
+src/lesson17/homework/asyncFlow.ts
+
++1 балл: Напишите async flow который сходит в https://swapi.dev/api/people и сохранит данные в стейте
++1 балл за обработку состояний реквеста в пути и ошибок
++1 балл за тесты
+
+*/
+
+// Action creators
+
+// Thunks
+
+// Reducer
---
+++ src/lesson17/homework/thunk.ts
@@ -0,0 +1,11 @@
+/*
+
+Курс React, урок 17: Middlewares
+
+Домашнее задание 2
+src/lesson17/homework/thunk.ts
+
++1 балл: Напишите свой thunk middleware
++1 балл за тесты
+
+*/
