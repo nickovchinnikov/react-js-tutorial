e7fe3cc6e2b473b5517a0957bc2ce0d0a189d88e
Michael Bashurov <saitonakamura@wisebits.com>
2020-06-01 13:22:06 +0300

    Enable logger middleware only in dev

--- src/rdx/store.ts
+++ src/rdx/store.ts
@@ -1,9 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunkMiddleware from "redux-thunk";
-import loggerMiddleware from "redux-logger";
 import { reducer } from "./reducer";
+
+const middleware = [thunkMiddleware];
+
+if (process.env.NODE_ENV === `development`) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { logger } = require(`redux-logger`);
+
+  middleware.push(logger);
+}
 
 export const store = configureStore({
   reducer,
-  middleware: [thunkMiddleware, loggerMiddleware],
+  middleware,
 });
