b01264082131f8d094a09898a4b5499cf6844510
Michael Bashurov <saitonakamura@wisebits.com>
2020-06-01 12:39:53 +0300

    Add middlewares

--- src/rdx/store.ts
+++ src/rdx/store.ts
@@ -1,6 +1,9 @@
-import { reducer } from "./reducer";
 import { configureStore } from "@reduxjs/toolkit";
+import thunkMiddleware from "redux-thunk";
+import loggerMiddleware from "redux-logger";
+import { reducer } from "./reducer";
 
 export const store = configureStore({
   reducer,
+  middleware: [thunkMiddleware, loggerMiddleware],
 });
