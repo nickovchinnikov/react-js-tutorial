ee16711be927bbfc1360ddab803ab3b7a42b0a10
Vasil Vanchuk <v.vanchuk@tut.by>
2020-06-24 22:19:38 +0300

    Fix test for authorized scenario

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,43 @@ import { Provider } from "react-redux";
 
 import { TicTacToeGame, Header, SignIn } from "./screens";
 
-import { store } from "./store";
+import { store, TicTacToeGameState } from "./store";
 
-export const App: React.FC<{}> = () => (
-  <Provider store={store}>
-    <Router>
-      <Header />
-      <nav>
-        <ul>
-          <li>
-            <Link to="/signin">SignIn</Link>
-          </li>
-          <li>
-            <Link to="/ticktacktoe">TickTackToe</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route path="/ticktacktoe">
-          <TicTacToeGame />
-        </Route>
-        <Route path="*">
-          <Redirect to="/signin" />
-        </Route>
-      </Switch>
-    </Router>
-  </Provider>
-);
+const selectIsAuthorized = (state: TicTacToeGameState): boolean =>
+  Boolean(state.login.username);
+
+export const App: React.FC<{}> = () => {
+  const state = store.getState();
+  const isAuthorized = selectIsAuthorized(state);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <nav>
+          <ul>
+            {!isAuthorized ? (
+              <li>
+                <Link to="/signin">SignIn</Link>
+              </li>
+            ) : null}
+            <li>
+              <Link to="/ticktacktoe">TickTackToe</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/ticktacktoe">
+            <TicTacToeGame />
+          </Route>
+          <Route path="*">
+            <Redirect to="/signin" />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
+  );
+};
