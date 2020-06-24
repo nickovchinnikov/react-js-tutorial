09d59aabdb0f74d1b44f6a87b8e5c6df0380ecbb
Vasil Vanchuk <v.vanchuk@tut.by>
2020-06-24 21:52:06 +0300

    Add test to check incorrect case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { store } from "./store";
 import { App } from "./App";
 import { shallow } from "enzyme";
+import { loginSlice } from "@/modules/Login";
 
 describe("App", () => {
   it('should show "Sign in" link by default', () => {
     const app = shallow(<App />);
     expect(app.find('Link[to="/signin"]').length).toBe(1);
+  });
+
+  it('should not show "Sign in" link for authorized user', () => {
+    store.dispatch(loginSlice.actions.login("some user"));
+    const app = shallow(<App />);
+    expect(app.find('Link[to="/signin"]').length).toBe(0);
   });
 });
