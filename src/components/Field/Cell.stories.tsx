918320b5b129b3cd381bb4d3739ead10001a2b81
NickOvchinnikov <nickovchinnikof@gmail.com>
2020-09-22 10:08:01 +0300

    MDX examples

--- src/components/Field/Cell.stories.tsx
+++ src/components/Field/Cell.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { action } from "@storybook/addon-actions";
 import { withKnobs, text, number } from "@storybook/addon-knobs";
 import { Cell } from "./Cell";
+
 export default {
   title: "Cell",
   decorators: [withKnobs],
