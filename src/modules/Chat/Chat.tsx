60fa228998f8b445956ecb088499eeb35f87e1a6
NickOvchinnikov <nickovchinnikof@gmail.com>
2020-10-04 10:31:57 +0300

    Add chat component

---
+++ src/modules/Chat/Chat.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useState } from "react";
+import { connect } from "react-redux";
+import { isEmpty } from "ramda";
+
+import { TicTacToeGameState } from "@/store";
+
+import { MessageComponent } from "./Message";
+import { actions } from "./reducer";
+
+const mapStateToProps = ({ chat, login }: TicTacToeGameState) => ({
+  chat,
+  ...login,
+});
+
+const mapDispatchToProps = {
+  send: actions.send,
+};
+
+export type Props = ReturnType<typeof mapStateToProps> &
+  typeof mapDispatchToProps;
+
+export const ChatComponent: React.FC<Props> = ({ chat, username, send }) => {
+  const [message, setMsg] = useState("");
+  const onSubmit = useCallback(
+    async (ev) => {
+      ev.preventDefault();
+      if (!isEmpty(message)) {
+        send({ message, author: username });
+      }
+    },
+    [message, username, send]
+  );
+  return !isEmpty(username) ? (
+    <>
+      {chat.map(({ author, message }) => (
+        <MessageComponent key={`${author}_${message}`} author={author}>
+          {message}
+        </MessageComponent>
+      ))}
+      <form onSubmit={onSubmit}>
+        <label>
+          Message:
+          <input
+            placeholder="Enter your msg"
+            value={message}
+            onChange={(ev) => setMsg((ev.target as HTMLInputElement).value)}
+            required
+            minLength={4}
+            maxLength={10}
+          />
+        </label>
+        <button>Send</button>
+      </form>
+    </>
+  ) : null;
+};
+
+export const Chat = connect(mapStateToProps, mapDispatchToProps)(ChatComponent);
