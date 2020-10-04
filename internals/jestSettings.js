1ec3a566f70b4ed3e7d213a8cbb90a2de0e24ee6
NickOvchinnikov <nickovchinnikof@gmail.com>
2020-10-04 16:32:45 +0300

    Fix test

--- internals/jestSettings.js
+++ internals/jestSettings.js
@@ -5,3 +5,17 @@ import serializer from "jest-emotion";
 Enzyme.configure({ adapter: new Adapter() });
 
 expect.addSnapshotSerializer(serializer);
+
+// https://github.com/facebook/jest/issues/3449#issuecomment-347337666
+class Worker {
+  constructor(stringUrl) {
+    this.url = stringUrl;
+    this.onmessage = () => undefined;
+  }
+
+  postMessage(msg) {
+    this.onmessage(msg);
+  }
+}
+
+window.Worker = Worker;
--- src/modules/Hash/reducer.test.ts
+++ src/modules/Hash/reducer.test.ts
@@ -1,8 +1,10 @@
+import faker from "faker";
+
 import { actions, reducer, initialState } from "./reducer";
 
 describe("Hash reducer", () => {
   it("Update hash action", () => {
-    const hash = "test";
-    expect(reducer(initialState, actions.update(hash))).toEqual({ hash });
+    const hash = faker.lorem.words();
+    expect(reducer(initialState, actions.update(hash))).toEqual(hash);
   });
 });
