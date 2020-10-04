337c4350bf7ce98d1ad5849dec867df00d162a04
NickOvchinnikov <nickovchinnikof@gmail.com>
2020-10-04 11:43:32 +0300

    Update redux datatypes

--- src/store.ts
+++ src/store.ts
@@ -5,11 +5,13 @@ import { getSagaExtension } from "redux-dynamic-modules-saga";
 import { gameSlice } from "./modules/InteractiveField";
 import { loginSlice, getLoginModule } from "./modules/Login";
 import { backgroundSlice, getBackgroundModule } from "./modules/Background";
+import { chatSlice } from "./modules/Chat";
 
 const reducer = combineReducers({
   login: loginSlice.reducer,
   game: gameSlice.reducer,
   background: backgroundSlice.reducer,
+  chat: chatSlice.reducer,
 });
 
 export type TicTacToeGameState = ReturnType<typeof reducer>;
