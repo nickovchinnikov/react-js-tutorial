190ab12a4fcbecd87903aa51d65658ce45d8a47d
NickOvchinnikov <nickovchinnikof@gmail.com>
2020-10-04 11:42:59 +0300

    Websocket flow fix

--- server/websocket.ts
+++ server/websocket.ts
@@ -8,11 +8,16 @@ const io = SocketIO(server);
 
 io.on("connection", (socket) => {
   console.warn("a user connected");
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.emit("a user connected");
+
+  socket.on("chatMessage", (msg) => {
+    console.warn("chatMessage", msg);
+    socket.broadcast.emit("chatMessage", msg);
   });
+
   socket.on("disconnect", () => {
     console.warn("user disconnected");
+    socket.emit("user disconnected");
   });
 });
 
