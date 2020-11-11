85d26306c0dc04006a6d81c3beebc71ff8d7c660
Vasil Vanchuk <v.vanchuk@tut.by>
2020-11-11 09:40:47 +0200

    Add new test to cover some 'incorrect' implemetations

--- src/utils/withOnChangeValue.test.tsx
+++ src/utils/withOnChangeValue.test.tsx
@@ -31,4 +31,20 @@ describe("withOnChangeValue", () => {
     wrapper.simulate("change", event);
     expect(onChangeValue).toHaveBeenCalledWith(event.target.value);
   });
+
+  it("calls both onChangeText", () => {
+    const WrappedComponent = withOnChangeValue(InputText);
+    const onChangeValue = jest.fn();
+    const onChange = jest.fn();
+
+    const wrapper = shallow(
+      <WrappedComponent onChangeValue={onChangeValue} onChange={onChange} />
+    );
+    const event = {
+      target: { value: "234" },
+    };
+    wrapper.simulate("change", event);
+    expect(onChangeValue).toHaveBeenCalledWith(event.target.value);
+    expect(onChange).toHaveBeenCalledWith(event);
+  });
 });
