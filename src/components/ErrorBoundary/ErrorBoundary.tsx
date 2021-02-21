f2c9b10cbdcbf6d0906252a5bf9eaf5cfb4a0149
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2021-02-21 21:10:18 +0300

    Refactoring ErrorBoundary test

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from "react";
+import React, { FC, Component, ReactNode } from "react";
 
 export interface Props {
   children: ReactNode;
@@ -8,22 +8,24 @@ export interface State {
   hasError: boolean;
 }
 
-export const ErrorMsgComponent = () => <h1>Something went wrong.</h1>;
+export const ErrorMsgComponent: FC = () => (
+  <h1 data-testid="error-component">Something went wrong.</h1>
+);
 
 export class ErrorBoundary extends Component<Props, State> {
   state = { hasError: false };
 
-  static getDerivedStateFromError() {
+  static getDerivedStateFromError(): State {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
-  componentDidCatch(error: Error, errorInfo: any) {
+  componentDidCatch(error: Error, errorInfo: never): void {
     // You can also log the error to an error reporting service
     console.error(error, errorInfo);
   }
 
-  render() {
+  render(): ReactNode {
     if (this.state.hasError) {
       // You can render any custom fallback UI
       return <ErrorMsgComponent />;
