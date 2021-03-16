3f804cddaf08bb16331c4262de6f590cbaf30d30
Aleksandr Schemelev <prophreaker@gmail.com>
2021-03-16 21:30:11 +0300

    fix: change .toBe on .toEqual for object equality test

--- lessons/module2/4_FunctionalProgramming_Old/homework/ramdaPureFunctions.test.ts
+++ lessons/module2/4_FunctionalProgramming_Old/homework/ramdaPureFunctions.test.ts
@@ -27,7 +27,7 @@ import {
 // test("parseQs", () => {
 //   const qs = "?page=2&pageSize=10&total=205&somethingElse=value";
 
-  expect(parseQs(qs)).toBe({
+  expect(parseQs(qs)).toEqual({
     page: "2",
     pageSize: "10",
     total: "205",
