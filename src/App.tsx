69b26ba07f824f2841615c6d297ae893527b00ee
Max <korobprog@gmail.com>
2022-04-26 22:21:42 +1000

    game сделал поле grid и симуляцию

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,21 @@ import React, { useState, useCallback } from "react";
 import produce from "immer"; //https://habr.com/ru/company/ruvds/blog/349492/
 import { useRef } from "react";
 
+// установка typescript https://create-react-app.dev/docs/adding-typescript/
+
 const numRows = 50;
 const numCols = 50;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [0, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
 
 const App: React.FC = () => {
   const [grid, setGrid] = useState(() => {
@@ -23,15 +36,42 @@ const App: React.FC = () => {
     if (!runningRef.current) {
       return;
     }
-    // simulate
-    setTimeout(runSimulation, 1000);
+    // runSimulation логика 
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1; //станет единицей или нулем
+            }
+          }
+        }
+      });
+    });
+    // runSimulation
+    setTimeout(runSimulation, 800); // запускаем  через ...N
   }, []);
 
   return (
     <>
       <button
         onClick={() => {
-          setRunning(!running);
+          setRunning(!running); //запускаем симуляцию
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
         }}
       >
         {running ? `stop` : `start`}
