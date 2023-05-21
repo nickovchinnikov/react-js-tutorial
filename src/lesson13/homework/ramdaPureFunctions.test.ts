30d89baf51e8fd7cf5568d8d81a7e280fb9f60cf
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-05-21 17:14:15 +0300

    задачи по ramda

--- src/lesson13/homework/ramdaPureFunctions.test.ts
+++ src/lesson13/homework/ramdaPureFunctions.test.ts
@@ -1,5 +1,4 @@
 test("dummy test", () => undefined);
-/*
 
 import {
   getTopName,
@@ -9,30 +8,37 @@ import {
   parseQs,
 } from "./ramdaPureFunctions";
 
-// });
+test("getTopName", () => {
+  const teams: Team[] = [
+    { name: "Lions", score: 5 },
+    { name: "Tigers", score: 4 },
+    { name: "Bears", score: 6 },
+    { name: "Monkeys", score: 2 },
+  ];
 
-// test("createQs", () => {
-//   const qsObj: QsObj = {
-//     page: "2",
-//     pageSize: "10",
-//     total: "205",
-//     somethingElse: "value",
-//   };
+  expect(getTopName(teams)).toBe("Bears");
+});
 
-//   expect(createQs(qsObj)).toBe(
-//     "?page=2&pageSize=10&total=205&somethingElse=value"
-//   );
-// });
+test("createQs", () => {
+  const qsObj: QsObj = {
+    page: "2",
+    pageSize: "10",
+    total: "205",
+    somethingElse: "value",
+  };
 
-// test("parseQs", () => {
-//   const qs = "?page=2&pageSize=10&total=205&somethingElse=value";
+  expect(createQs(qsObj)).toBe(
+    "?page=2&pageSize=10&total=205&somethingElse=value"
+  );
+});
 
-  expect(parseQs(qs)).toBe({
+test("parseQs", () => {
+  const qs = "?page=2&pageSize=10&total=205&somethingElse=value";
+
+  expect(parseQs(qs)).toStrictEqual({
     page: "2",
     pageSize: "10",
     total: "205",
     somethingElse: "value",
   });
 });
-
-*/
--- src/lesson13/homework/ramdaPureFunctions.ts
+++ src/lesson13/homework/ramdaPureFunctions.ts
@@ -1,23 +1,68 @@
-// import {
-//   compose,
-//   //...
-// } from "ramda";
+import {
+  compose,
+  curry,
+  map,
+  reduce,
+  join,
+  toPairs,
+  concat,
+  replace,
+  split,
+} from "ramda";
 
 // // Задание 1
-// export type Team = { name: string; score: number };
+export type Team = { name: string; score: number };
+
+const getTeamByMaxScore = (maxTeam: Team, item: Team): Team => {
+  if (item.score > maxTeam.score) {
+    return item;
+  } else {
+    return maxTeam;
+  }
+};
+
+const trace = curry((tag, x) => {
+  console.log(tag, x);
+  return x;
+});
+
+const getTeamName = (item: Team): string => item.name;
 
 // eslint-disable-next-line
 // @ts-ignore
-export const getTopName = compose(/* ... */);
+export const getTopName = compose(
+  getTeamName,
+  reduce(getTeamByMaxScore, { name: "", score: 0 })
+);
 
 // // Задание 2
-// export type QsObj = Record<string, string | number | boolean | object>;
+export type QsObj = Record<string, string | number | boolean | object>;
 
 // eslint-disable-next-line
 // @ts-ignore
-export const createQs = compose(/* ... */);
+export const createQs = compose(
+  concat("?"),
+  join("&"),
+  map(join("=")),
+  toPairs
+);
 
 // eslint-disable-next-line
 // @ts-ignore
 // Задание 3
-export const parseQs = compose(/* ... */);
+
+const show = x => x;
+
+const reduceIterator = (acc: object, elem: object) => {
+  // @ts-ignore
+  acc[elem[0]] = elem[1];
+  return acc;
+};
+
+export const parseQs = compose(
+  show,
+  reduce(reduceIterator, {}),
+  map(split("=")),
+  split("&"),
+  replace("?", "")
+);
