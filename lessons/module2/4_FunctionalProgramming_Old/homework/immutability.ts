4615c0ccf6e3eb3bd803427a3373379da201b901
Evgeny Medvedev <medvedevea@elfin.tech>
2023-05-24 17:47:21 +0300

    completed hw

--- lessons/module2/4_FunctionalProgramming_Old/homework/immutability.ts
+++ lessons/module2/4_FunctionalProgramming_Old/homework/immutability.ts
@@ -1,39 +1,50 @@
 // // Задание 1
-// export type OriginalTeam = {
-//   size: number;
-//   name: string;
-//   league: string;
-// };
-
-// export type ExpectedTeam = {
-//   name: string;
-//   league: string;
-//   roster: number;
-// };
-
-// export const originalTeamToExpectedTeam = (
-//   originalTeam: OriginalTeam
-// ): ExpectedTeam => {
-//   //
-// };
-
-// // Задание 2
-// type SomeArray = Array<number | string>;
-
-// const originalArrayToExpectedArray = (originalArray: SomeArray): SomeArray => {
-//   //
-// };
-
-// // Задание 3
-
-// export type Team = {
-//   name: string;
-//   captain: {
-//     name: string;
-//     age: number;
-//   };
-// };
-
-// export const originalTeamToExpectedTeam = (originalTeam: Team): Team => {
-//   //
-// };
+export type OriginalTeam = {
+    size: number;
+    name: string;
+    league: string;
+  };
+  
+  export type ExpectedTeam = {
+    name: string;
+    league: string;
+    roster: number;
+  };
+  
+  export const originalTeamToExpectedTeam = (
+    originalTeam: OriginalTeam
+  ): ExpectedTeam => {
+    const expected = { name: "New York Badgers", roster: 25 };
+    const newTeam = { ...originalTeam, ...expected };
+    const { size, ...expectedTeam } = newTeam;
+    return expectedTeam;
+  };
+  
+  // // Задание 2
+  type SomeArray = (number | string)[];
+  
+  export const originalArrayToExpectedArray = (
+    originalArray: Readonly<SomeArray>
+  ): SomeArray => {
+    const newArray = [...originalArray].splice(1);
+    newArray.push(5);
+    newArray[0] = "two";
+    return newArray;
+  };
+  
+  // // Задание 3
+  
+  export type Team = {
+    name: string;
+    captain: {
+      name: string;
+      age: number;
+    };
+  };
+  
+  export const originalTeamToExpectedTeamDeep = (originalTeam: Team): Team => {
+    const newTeam = JSON.parse(JSON.stringify(originalTeam));
+    newTeam.captain.age = 28;
+    return newTeam;
+  };
+  
--- lessons/module2/4_FunctionalProgramming_Old/homework/pureFunctions.ts
+++ lessons/module2/4_FunctionalProgramming_Old/homework/pureFunctions.ts
@@ -1,22 +1,49 @@
 // // Задание 1
-// export type Team = { name: string; score: number };
+export type Team = { name: string; score: number };
 
-// export const getTopName = (teams: Team[]): string => {
-//   //
-// };
+export const getTopName = (teams: Team[]): string =>
+  [...teams].sort((team1, team2) => team2.score - team1.score)[0].name;
 
-// // Задание 2
-export type QsObj = Record<
-  string,
-  string | number | boolean | string[] | number[] | boolean[]
->;
+// Задание 2
+export type QsObjValue =
+  | string
+  | number
+  | boolean
+  | (string | number | boolean)[];
 
-// export const createQs = (qsObj: QsObj): string => {
-//   //
-// };
+export type QsObj = Record<string, QsObjValue>;
 
-// // Задание 3
+export const createQs = (qsObj: QsObj): string => {
+  const formatValue = (value: QsObjValue): string => {
+    if (Array.isArray(value)) {
+      return `[${value.toString()}]`;
+    } else return value.toString();
+  };
 
-// export const parseQs = (qs: string): QsObj => {
-//   //
-// };
+  const queryArray: string[] = [];
+  Object.keys(qsObj).map((e) =>
+    queryArray.push(`${e}=${formatValue(qsObj[e])}`)
+  );
+  return `?${queryArray.join("&")}`;
+};
+
+// Задание 3
+
+export const parseQs = (qs: string): QsObj => {
+  const parseArray = (arr: string[]): string[] | [][] => {
+    if (arr[1].match(/\[(.*?)\]/)) {
+      arr[1] = JSON.parse(arr[1]);
+    }
+    return arr;
+  };
+
+  const qsObj = Object.fromEntries(
+    qs
+      .substring(1)
+      .split("&")
+      .map((e) => e.split("="))
+      .map((e) => parseArray(e))
+  );
+
+  return qsObj;
+};
