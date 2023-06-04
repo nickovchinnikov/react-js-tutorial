cf9232028763e93c232599118bdcdb181fbf8627
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-06-04 17:21:22 +0300

    поправил задание 3

--- lessons/module2/1_TypescriptPart2/homework/easy3.ts
+++ lessons/module2/1_TypescriptPart2/homework/easy3.ts
@@ -3,13 +3,16 @@
 // Нужно заменить FIXME на соответствующий тип
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = Record<any, any>;
+type FIXME<T extends Record<any, any>, K extends keyof T> = Pick<
+  T,
+  Exclude<keyof T, K>
+>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const omit = <T extends Record<any, any>, K extends keyof T>(
   obj: T,
   keyToOmit: K
-): FIXME => {
+): FIXME<T, K> => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { [keyToOmit]: _, ...withoutKey } = obj;
   return withoutKey;
